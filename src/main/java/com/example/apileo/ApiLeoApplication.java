7ff1bfdf64143d23eaa5830ee79f7fdc78aa1883
Leonardo de Souza <leosz.souza@gmail.com>
2020-02-13 09:32:32 -0300

    procfile

--- src/main/java/com/example/apileo/ApiLeoApplication.java
+++ src/main/java/com/example/apileo/ApiLeoApplication.java
@@ -3,10 +3,11 @@ package com.example.apileo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ApiLeoApplication {
 
 	public static void main(String[] args) {
