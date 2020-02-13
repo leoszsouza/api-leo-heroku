a59da0686588a1dfbf779a17bb399340839e3379
Leonardo de Souza <leosz.souza@gmail.com>
2020-02-13 10:55:43 -0300

    procfile

--- src/main/java/com/example/apileo/MongoConfiguration.java
+++ src/main/java/com/example/apileo/MongoConfiguration.java
@@ -1,24 +1,26 @@
 package com.example.apileo;
 
-import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
 
 @Configuration
-public class MongoConfiguration extends AbstractMongoClientConfiguration {
+public class MongoConfiguration  {
 
 
-    @Override
-    protected String getDatabaseName() {
-        return "heroku_lr58z8b1";
+    public @Bean
+    MongoClient mongoClient() {
+        return new MongoClient("localhost") {
+        };
     }
 
-    @Override
-    public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017/?replicaSet=rs0&w=majority");
+    public @Bean
+    MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "heroku_lr58z8b1");
     }
 }
